029004708f566ff60b312e687c256e1017228c0d
Hatim Tai <hatim009@users.noreply.github.com>
2023-12-05 14:58:24 +0530

    Added farmer update API

--- farmers/serializers.py
+++ farmers/serializers.py
@@ -34,6 +34,31 @@ class FarmerSerializer(serializers.ModelSerializer):
             validated_data['profile_photo'] = self.fields['profile_photo'].create(validated_data['profile_photo'])
             validated_data['id_front_image'] = self.fields['id_front_image'].create(validated_data['id_front_image'])
             validated_data['id_back_image'] = self.fields['id_back_image'].create(validated_data['id_back_image'])
+            
             farmer = Farmer(**validated_data)
             farmer.save()
+            
             return farmer
+    
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            if validated_data.get('profile_photo'):
+                self.fields['profile_photo'].update(instance.profile_photo, validated_data['profile_photo'])
+            if validated_data.get('id_front_image'):
+                self.fields['id_front_image'].update(instance.id_front_image, validated_data['id_front_image'])
+            if validated_data.get('id_back_image'):
+                self.fields['id_back_image'].update(instance.id_back_image, validated_data['id_back_image'])
+            
+            instance.id_hash = validated_data.get('id_hash', instance.id_hash)
+            instance.name = validated_data.get('name', instance.name)
+            instance.guardian_name = validated_data.get('guardian_name', instance.guardian_name)
+            instance.date_of_birth = validated_data.get('date_of_birth', instance.date_of_birth)
+            instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+            instance.category = validated_data.get('category', instance.category)
+            instance.gender = validated_data.get('gender', instance.gender)
+            instance.village = validated_data.get('village', instance.village)
+            instance.income_level = validated_data.get('income_level', instance.income_level)
+
+            instance.save()
+            
+            return instance

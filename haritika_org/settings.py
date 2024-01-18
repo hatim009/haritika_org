a27387dfe09a8c97478c03495d23505eb9cf880b
Hatim Tai <hatim009@users.noreply.github.com>
2024-01-18 11:38:41 +0530

    Added settings to read config from environment variable

--- haritika_org/settings.py
+++ haritika_org/settings.py
@@ -134,11 +134,11 @@ WSGI_APPLICATION = 'haritika_org.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'haritika-org-db',
-        'USER': 'haritikaorgadmin',
-        'PASSWORD': 'AgRt&125$JdTh^895',
-        'HOST': 'haritika-org-db.cpkq2cy6abf7.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
 

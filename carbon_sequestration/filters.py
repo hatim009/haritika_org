6a8c67e607ac857cd4646d26123a12ed3b387062
Hatim Tai <hatim009@users.noreply.github.com>
2024-03-04 13:39:30 +0530

    Fixed search and filter functionalities for carbon_sequestration https://github.com/de2si/gatherer/issues/7

--- carbon_sequestration/filters.py
+++ carbon_sequestration/filters.py
@@ -5,10 +5,10 @@ from filters import MultiValueFilter
 
 
 class CarbonSequestrationFilter(filters.FilterSet):
-    villages = MultiValueFilter(field_name='village', lookup_expr='in')
-    blocks = MultiValueFilter(field_name='village', lookup_expr='block__in')
-    districts = MultiValueFilter(field_name='village', lookup_expr='block__district__in')
-    states = MultiValueFilter(field_name='village', lookup_expr='block__district__state__in')
+    villages = MultiValueFilter(field_name='land_parcel', lookup_expr='village__in')
+    blocks = MultiValueFilter(field_name='land_parcel', lookup_expr='village__block__in')
+    districts = MultiValueFilter(field_name='land_parcel', lookup_expr='village__block__district__in')
+    states = MultiValueFilter(field_name='land_parcel', lookup_expr='village__block__district__state__in')
     
     class Meta:
         model = CarbonSequestration
--- carbon_sequestration/views.py
+++ carbon_sequestration/views.py
@@ -7,13 +7,30 @@ from carbon_sequestration.serializers import CarbonSequestrationSerializer, Carb
 from carbon_sequestration.models import CarbonSequestration
 from carbon_sequestration.filters import CarbonSequestrationFilter
 from .permissions import IsAdmin, IsSupervisor, IsSurveyor
+from users.models import User
+from local_directories.models import VillagesDirectory
+
 
 class CarbonSequestrationViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsAdmin|IsSupervisor|IsSurveyor]
     serializer_class = CarbonSequestrationSerializer
     queryset = CarbonSequestration.objects.all()
     filterset_class = CarbonSequestrationFilter
-    search_fields = ['land_parcel__farmer', 'land_parcel__framer__phone_number', 'land_parcel__framer__id_hash', 'land_parcel__framer__name', 'land_parcel']
+    search_fields = ['land_parcel__farmer__farmer_id', 'land_parcel__farmer__phone_number', 'land_parcel__farmer__id_hash', 'land_parcel__farmer__name', 'land_parcel__id']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.action == 'list' and 'search' not in self.request.query_params:
+            assigned_villages = None
+            if self.request.user.user_type != User.UserType.ADMIN:
+                assigned_blocks = [user_block.block for user_block in self.request.user.assigned_blocks.all()]
+                assigned_villages = VillagesDirectory.objects.filter(block__in=assigned_blocks)
+
+            if assigned_villages:
+                queryset = queryset.filter(land_parcel__village__in=assigned_villages)
+
+        return queryset
 
     @action(detail=True, methods=['put'], name='Update model progress')
     def progress(self, request, pk=None):

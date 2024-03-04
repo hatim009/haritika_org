f8c2493a9dbc0e894591dd2958110a9689c6ce7f
Hatim Tai <hatim009@users.noreply.github.com>
2024-03-04 18:20:24 +0530

    Changes to restrict projects list view to only assigned projects https://github.com/de2si/gatherer/issues/13

--- projects/views.py
+++ projects/views.py
@@ -4,13 +4,15 @@ from rest_framework.response import Response
 from .models import Project
 from .serializers import ProjectSerializer
 from .permissions import IsAdmin, IsSupervisor, IsSurveyor
+from users.models import User
 from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated, IsAdmin|IsSupervisor|IsSurveyor])
 def list_projects(request):
-    projects = Project.objects.all().order_by('id')
+    assigned_projects = set([user_project_block.project.id for user_project_block in request.user.assigned_projects.all()])
+    projects = [project for project in Project.objects.all().order_by('id') if request.user.user_type == User.UserType.ADMIN or project.id in assigned_projects]
     project_serializer = ProjectSerializer(projects, many=True)
     return Response({'projects': project_serializer.data})
 
